aefa778317df9e52cfbb8c583c7f18d4867ebe87
Haowei Lee <haoweilee.tpe@gmail.com>
2022-08-25 23:14:50 +0200

    feat: complete guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
@@ -18,15 +19,18 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("{}", "Too small".red()),
+            Ordering::Greater => println!("{}", "Too big".red()),
             Ordering::Equal => {
-                println!("You win");
+                println!("{}", "You win".green());
                 break;
             }
         }
